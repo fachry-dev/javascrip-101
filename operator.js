f901941f8442efa590eb70f6c0e2b3ceef58bec6
fachry-dev <fai.rizky.w@gmail.com>
2024-08-22 09:30:10 +0700

    directly

---
+++ operator.js
@@ -0,0 +1,83 @@
+/*
+Operator
+1.Arithmatic
+2.logical
+3.Assigment
+*/
+
+/*
+Arithmatic
+1.Angka
+2.Operasi Aritmatika
+
+Operator 
+1. +  Edition
+2. -  Substraction
+3. /  Divison
+4. *  Multiplication
+5.. ++ Increment (penjumlahan  1 angka )
+6. --  Decrement ( Pengurangan 1 angka )
+7. ** Expoonential
+8. % Modulus
+*/ 
+
+
+
+
+
+/* contoh Edition
+let angka =5;
+angka ++; //6
+angka + 1; //6
+
+Contoh Substraction
+angka --; // 4
+angka -1; // 4
+
+contoh Exponential
+angka **2
+3**4
+
+ binary  
+ +, -,/,*,**,%
+
+ Unary 
+ ++, --
+
+ Assigment -- Assign Value
+ 1.=
+ 2.+=
+ 3.-=
+ 4.*=
+ 5./=
+ 6.**=
+
+Tenary -- Pengondisian
+abc?"123" : "321"
+*/
+
+// Operand
+let angka1 = 10;
+angka1 = angka1 + 20 * 99; // 10 + 20 
+console.log(angka1);
+
+
+/*
+Logical - Logika
+
+Operator :
+1. !  Negation (tidak sama dengan)
+2. && and
+3. || Or
+
+operasi logika matematika
+*/
+
+
+
+Boolean
+true/false
+
+
+let Assigment =10
+let AssigmentResult
