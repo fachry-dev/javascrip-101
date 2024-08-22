3e9c4e71884c1fa3df5c19ff5d2184f75dfd4815
fachry-dev <fai.rizky.w@gmail.com>
2024-08-22 09:04:21 +0700

    day 4: index

---
+++ variabel.js
@@ -0,0 +1,34 @@
+//ini contoh komentar
+// shortcutnya CTRL+ ? single line
+/*
+*/
+
+/*
+let asd ="ABD";
+asd =123;
+
+console.log(angka);
+
+const
+1.tidak bisa diubah 
+2.tidak bisa di deklarasi ;
+
+const angka = 123;
+
+/*
+let
+1.Bisa diganti valuenya 
+2.tidak bisa di deklarasi ulang
+*/
+
+/*
+Const
+1.valuenya tetap/diganti valuenya
+2.tidak bisa di deklarasi ulang
+
+//inisiasi
+let variabel 2 = 1233;
+
+//Deklarasi
+let variabel ;
+*/
